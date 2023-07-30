cc4207f51f4c008b71b892591c425373076071e7
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-30 21:17:47 +0200

    App1

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <header>
-    {{-- <div class="container">
+    <div class="container">
       <nav>
         <h2>BlogPage</h2>
         <ul>
@@ -18,7 +18,7 @@
           <li><a href="">Login</a></li>
         </ul>
       </nav>
-    </div> --}}
+    </div>
 </header>
 </body>
 </html>
